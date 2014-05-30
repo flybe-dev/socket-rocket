b98caff8c7eb242f31f1a78f4941c2036fddeacf
Jamie Bowdidge <jamie.bowdidge@flybe.com>
2014-05-30 15:59:15 +0100

    Ooops...obligatory second commit

--- project.clj
+++ project.clj
@@ -6,6 +6,4 @@
   :repositories ^:replace [["artifactory" "http://repo.flybe.local/artifactory/repo"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.taoensso/timbre "3.1.6"]
-                 [server-socket "1.0.0"]
-                 [congomongo "0.4.3"]
-                 [com.flybe/flybe-json "0.1.0-SNAPSHOT"]])
+                 [cheshire "5.3.1"]])
